77c564c99c868f0a8740e6aa0d1799ef95d0e536
bhushan2196 <140087711+bhushan2196@users.noreply.github.com>
2023-07-24 17:11:48 +0530

    Add files via upload

---
+++ sketch__1.py
@@ -0,0 +1,18 @@
+# # code for local image
+
+# from sketchpy import canvas
+# obj = canvas.sketch_from_image('D:\coding\python\portrait-art-robert-downey-jr-wallpaper-preview.jpg')
+# obj.draw(threshold=110)
+
+
+# code for apj
+
+# from sketchpy import library
+# myObject = library.apj()
+# myObject.draw()
+
+# code for rdj
+
+from sketchpy import library
+myObject = library.rdj()
+myObject.draw()
